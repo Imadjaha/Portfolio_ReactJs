35b2836d9c9b8a1d2dfc28019632cee8800a78ab
Aimad Bouchouaf <2226197.@uni-wuppertal.de>
2024-05-31 19:01:09 +0200

    Added filter to the projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,51 @@
 import React from "react";
 import PROJECTS from "../data/projects";
+import { useState } from "react";
+import "../index.css";
 
 const Projects = () => {
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const categories = [
+  
+    ...new Set(PROJECTS.flatMap((PROJECT) => PROJECT.catategory)),
+  ];
+
+  const toggleCategory = (category) => {
+    setSelectedCategories((prevSelectedCategories) =>
+      prevSelectedCategories.includes(category)
+        ? prevSelectedCategories.filter((c) => c !== category)
+        : [...prevSelectedCategories, category]
+    );
+  };
+
+  const filteredProjects =
+    selectedCategories.length === 0
+      ? PROJECTS
+      : PROJECTS.filter((project) =>
+          selectedCategories.every((category) =>
+            project.catategory.includes(category)
+          )
+        );
+        
   return (
+
     <div>
       <h2>My Projects</h2>
       <div>
-        {PROJECTS.map((PROJECT) => {
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => toggleCategory(category)}
+            className={selectedCategories.includes(category) ? "selectedProjects" : "projects"}
+        
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+      <div>
+        {filteredProjects.map((PROJECT) => {
           return <Project key={PROJECT.id} project={PROJECT} />;
         })}
       </div>
@@ -14,24 +53,25 @@ const Projects = () => {
   );
 };
 
-const Project = (props) => {
-   
 
-    const {title, description, image , link } = props.project; // means     let title = props.project.title;
-                                                                        // let description = props.project.description;
-                                                                        // let image = props.project.image;
-                                                                        // let link = props.project.link;
+const Project = (props) => {
+  const { title, description, image, link } = props.project; // means     let title = props.project.title;
+  // let description = props.project.description;
+  // let image = props.project.image;
+  // let link = props.project.link;
   return (
     <div className="card">
       <img src={image} alt="Project1 puic" style={{ width: "100%" }} />
       <h3 style={{ color: "grey", fontSize: 20 }}>{title}</h3>
-      <p style={{ fontSize: 17, padding: 4 }}>
-       {description}
-      </p>
-      <button onClick={() => {
-       // window.location.href = link;
-        window.open(link, '_blank');
-      }} >Take A Look</button>
+      <p style={{ fontSize: 17, padding: 4 }}>{description}</p>
+      <button
+        onClick={() => {
+          // window.location.href = link;
+          window.open(link, "_blank");
+        }}
+      >
+        Take A Look
+      </button>
     </div>
   );
 };
--- src/data/projects.js
+++ src/data/projects.js
@@ -12,56 +12,65 @@ const PROJECTS = [
         title: "Guess The Word Game",
         description: "Play 'Guess the Word Game' - a dynamic, color coded challenge to test your word prowess!",
         link: "https://imadjaha.github.io/GuessTheWordGame/",
-        image: project1
+        image: project1,
+        catategory: ["JavaScript", "HTML", "CSS"]
     },
     {
         id: 2,
         title: "TO-DO List",
         description: "Create a To-Do List with HTML, CSS & JS, utilizing local storage for persistent data.",
         link: "https://imadjaha.github.io/To-Do-List/",
-        image: project2
+        image: project2,
+        catategory: ["JavaScript", "HTML", "CSS"]
     },
     {
         id: 3,
         title: "Calculator",
         description: "Mini Calculator: Modern tool for daily math needs, with sleek design & intuitive UI.",
         link: "https://imadjaha.github.io/calculator/",
-        image: project3
+        image: project3,
+        catategory: ["JavaScript", "HTML", "CSS"]
     },
     {
         id: 4,
         title: "Tic Tac Toe",
         description: "Tic Tac Toe reimagined: Next.js React app with immersive gameplay and polished design.",
         link: "https://imadjaha.github.io/tic-tac-toe-ReactJs_NextJs/",
-        image: project4
+        image: project4,
+        catategory: ["ReactJs", "NextJs","Typescript","CSS"]
+
     },
     {
         id: 5,
         title: "Weather App",
         description: "Get real-time weather information with an easy-to-use interface. Built with ReactJs.",
         link: "https://imadjaha.github.io/Weather-App/",
-        image: project5
+        image: project5,
+        catategory: ["ReactJs", "JavaScript", "CSS"]
     },
     {
         id: 6,
         title: "Function Diagram",
         description: " Web app for plotting math functions, offering intuitive interface & export options.",
         link: "https://imadjaha.github.io/Function_Diagram/",
-        image: project6
+        image: project6,
+        catategory: ["JavaScript", "HTML", "CSS"]
     },
     {
         id: 7,
         title: "Hangman Game",
         description: "A classic Hangman game created with ReactJs Vite as platform-agnostic frontend.",
         link: "https://imadjaha.github.io/Hangman/",
-        image: project7
+        image: project7,
+        catategory: ["ReactJs", "Typescript", "CSS"]
     },
     {
         id: 8,
         title: "Snake Game",
         description: "A classic Snake game from 19th century. created with Python.",
         link: "https://github.com/Imadjaha/snake-game",
-        image: project8
+        image: project8,
+        catategory: ["Python"]
     }
 
 ]
