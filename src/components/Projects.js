4fc4a59f64052cb02be597569bf29460e698c8fc
Aimad Bouchouaf <2226197.@uni-wuppertal.de>
2024-05-31 23:56:36 +0200

    Added Darkmode

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,12 @@
 import React from "react";
 import PROJECTS from "../data/projects";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../index.css";
 
 const Projects = () => {
   const [selectedCategories, setSelectedCategories] = useState([]);
 
   const categories = [
-  
     ...new Set(PROJECTS.flatMap((PROJECT) => PROJECT.catategory)),
   ];
 
@@ -27,9 +26,8 @@ const Projects = () => {
             project.catategory.includes(category)
           )
         );
-        
-  return (
 
+  return (
     <div>
       <h2>My Projects</h2>
       <div>
@@ -37,36 +35,40 @@ const Projects = () => {
           <button
             key={category}
             onClick={() => toggleCategory(category)}
-            className={selectedCategories.includes(category) ? "selectedProjects" : "projects"}
-        
+            className={
+              selectedCategories.includes(category)
+                ? "selectedProjects"
+                : "projects"
+            }
           >
             {category}
           </button>
         ))}
       </div>
       <div>
-        {filteredProjects.map((PROJECT) => {
-          return <Project key={PROJECT.id} project={PROJECT} />;
-        })}
+        {filteredProjects.map((PROJECT) => (
+          <Project key={PROJECT.id} project={PROJECT} />
+        ))}
       </div>
     </div>
   );
 };
 
-
 const Project = (props) => {
-  const { title, description, image, link } = props.project; // means     let title = props.project.title;
-  // let description = props.project.description;
-  // let image = props.project.image;
-  // let link = props.project.link;
+  const { title, description, image, link } = props.project;
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    setIsShown(true);
+  }, []);
+
   return (
-    <div className="card">
-      <img src={image} alt="Project1 puic" style={{ width: "100%" }} />
+    <div className={`card ${isShown ? "show" : ""}`}>
+      <img src={image} alt="Project1 pic" style={{ width: "100%" }} />
       <h3 style={{ color: "grey", fontSize: 20 }}>{title}</h3>
       <p style={{ fontSize: 17, padding: 4 }}>{description}</p>
       <button
         onClick={() => {
-          // window.location.href = link;
           window.open(link, "_blank");
         }}
       >
